76b53588c398c79097b855b0cebb9ee05d272d5d
Cristian Bedoya <cristianbedoyavargas@gmail.com>
2023-03-21 15:23:05 -0500

    Entity class updated

--- DDBasedChallenge.Domain/Abstract/Entity.cs
+++ DDBasedChallenge.Domain/Abstract/Entity.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 
 namespace DDDBasedChallenge.Domain.Abstract
 {
-    public class Entity : IEntity
+    public abstract class Entity
     {
-        public int Id { get; set; }
+        public int Id { get; protected set; }
+        public DateTime CreationDate { get; protected set; }
+        public DateTime UpdatedDate { get; protected set; }
+        public bool IsDeleted { get; protected set; }
     }
 }
